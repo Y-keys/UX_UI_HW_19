eb025039cbe2528be418fceed4067c2d54dc2aac
y-keys <104732867+y-keys@users.noreply.github.com>
2022-10-22 14:50:54 -0600

    added a js function to the top nav bar to hide and show on scroll

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
-//js function for top nav menu to be responsive
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+//js function for the responsive top nav menu on each page
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
+//js function for hinding the top nav menu on scroll on each page
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myTopnav").style.top = "0";
+  } else {
+    document.getElementById("myTopnav").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 //js function for the study case page
 function openStep(evt, title) {
